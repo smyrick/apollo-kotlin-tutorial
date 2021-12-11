55803ba290b3cef2ea8d3e6f1094ebada0fc15ae
Martin Bonnin <martin@mbonnin.net>
2021-12-11 01:46:50 +0100

    make it work with the non-shadowed plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,15 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        mavenLocal()
     }
 
     dependencies {
+        classpath("org.antlr:antlr4:4.9.3")
         classpath("com.android.tools.build:gradle:4.1.3")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.5")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+        classpath("com.apollographql.apollo3:apollo-gradle-plugin:3.0.0-rc03-SNAPSHOT")
     }
 }
 
@@ -15,6 +18,7 @@ allprojects {
     repositories {
         google()
         mavenCentral()
+        mavenLocal()
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,5 +5,6 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
+        mavenLocal()
     }
 }
